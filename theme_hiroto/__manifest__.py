aafc034ab67df93463e2c317689771ebd6bb3fb0
Gracias Kasongo <graciaskas96@gmail.com>
2025-11-19 14:51:57 +0200

    [ADD] : Theme Hiroto from colorlib

---
+++ theme_hiroto/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Hiroto',
+    'description': 'A description for your theme.',
+    'version': '1.0',
+    'author': 'ZeSlap Platforms,Gracias Kasongo',
+    'category': 'Theme/Services',
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+        "views/assets.xml",
+        "views/layout.xml",
+        "views/snippets.xml",
+        "views/pages/index.xml",
+        "views/pages/rooms.xml"
+    ],
+}
